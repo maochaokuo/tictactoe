21013557292be7087ce40900089e822ff5794661
Thomas Mao <tm1813@gmail.com>
2021-05-27 22:34:50 +0800

    update a few progress

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -5,23 +5,28 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/tictactoe/core.cljs. Go ahead and edit it and see reloading in action.")
+(println "I changed some text")
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state 
+  (atom {:text "Welcome to tic tac toe"}))
 
 
-(defn hello-world []
+(defn tictactoe []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:svg 
+    {:view-box "0 0 30 30"
+     :width 500
+     :height 500}
+    [:circle {:r 30 :cx 30 :cy 30}]]])
 
-(rd/render [hello-world]
+(rd/render [tictactoe]
            (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  (swap! app-state assoc-in [:text] "Hi")
 )
